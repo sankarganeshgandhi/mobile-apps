4ae4c18a7cab314c180e54a75d7c24afff66d18f
sankarganeshgandhi <sankarganesh.gandhi@gmail.com>
2019-01-24 18:20:17 +0000

    Initial commit

--- examples/flutter-apps/startup_namer/lib/main.dart
+++ examples/flutter-apps/startup_namer/lib/main.dart
@@ -28,14 +28,50 @@ class MyApp extends StatelessWidget {
 class RandomWordsState extends State {
   // TODO Add build() method
   final _suggestions = <WordPair>[];
+  final Set<WordPair> _savedWordPairs = new Set<WordPair>();
   final _biggerFont = const TextStyle(fontSize: 18.0);
 
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Startup Name Generator'),
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.list), onPressed: _pushSaved
+          ),
+        ],
       ),
       body: _buildSuggestions(),
+    );
+  }
+
+  void _pushSaved() {
+    Navigator.of(context).push(
+      new MaterialPageRoute<void>(
+          builder: (BuildContext context) {
+            final Iterable<ListTile> tiles = _savedWordPairs.map(
+                (WordPair pair) {
+                  return new ListTile(
+                    title: new Text(
+                      pair.asPascalCase,
+                      style: _biggerFont,
+                    ),
+                  );
+                }
+            );
+            final List<Widget> divided = ListTile
+                .divideTiles(
+                  context: context,
+                  tiles: tiles,
+                ).toList();
+            return new Scaffold(
+              appBar: new AppBar(
+                title: const Text('Saved Suggestions'),
+              ),
+              body: new ListView(children: divided),
+            );
+          },
+      ),
     );
   }
 
@@ -55,11 +91,26 @@ class RandomWordsState extends State {
   }
 
   Widget _buildRow(WordPair wordPair) {
+    final bool alreadySaved = _savedWordPairs.contains(wordPair);
+
     return ListTile(
       title: Text(
         wordPair.asPascalCase,
         style: _biggerFont,
-      )
+      ),
+      trailing: new Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+      ),
+      onTap: () {
+        setState(() {
+          if (alreadySaved) {
+            _savedWordPairs.remove(wordPair);
+          } else {
+            _savedWordPairs.add(wordPair);
+          }
+        });
+      },
     );
   }
 
